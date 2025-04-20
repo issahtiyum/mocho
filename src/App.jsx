f17512f8e020b494baadfb8d825cdd181f13e745
Haris Issah <myharis.issah@gmail.com>
2025-04-20 08:10:39 +0000

    Build initial ui of the page

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <>
+      <h1>Mocho</h1>
+      <div className="upload-container">
+        <img src="./camera-upload.svg" alt="" />
+        <p>Click to upload an image</p>
+      </div>
+    </>
+  );
+}
