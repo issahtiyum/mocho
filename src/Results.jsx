049e8c649323a2172411f52bad486ce5860726bd
Haris Issah <myharis.issah@gmail.com>
2025-04-23 12:18:47 +0000

    Second iteration of UI changes

--- src/Results.jsx
+++ src/Results.jsx
@@ -1,16 +1,42 @@
 export default function Results({ results }) {
   return (
     <div className="results-container">
-      <div className="result-item">{results.description}</div>
-      <div className="result-item">
-        Estimated Calories: {results.estimatedCalories}
+      <h2 className="result-item description">{results.description}</h2>
+      <div className="figures">
+        <div className="result-item estimated-calories">
+          <div>
+            <span>Calories</span>
+            <span>🔥</span>
+          </div>
+          <span>{results.estimatedCalories}</span>
+        </div>
+        <div className="result-item estimated-mass">
+          <div>
+            <span>Mass</span>
+            <span>⚖️</span>
+          </div>
+          <span>{results.estimatedMass}</span>
+        </div>
+        <div className="result-item oil-content">
+          <div>
+            <span>Oil Content</span>
+            <span>🍳</span>
+          </div>
+          <span>{results.oilContent}</span>
+        </div>
+        <div className="result-item confidence">
+          <div>
+            <span>Accuracy</span>
+            <span>💪</span>
+          </div>
+          <span>{results.confidence * 100}%</span>
+        </div>
+        {/* <div className="result-item health-rating">
+        Health Rating: {results.healthRating}
+      </div> */}
       </div>
-      <div className="result-item">Estimated Mass: {results.estimatedMass}</div>
-      <div className="result-item">Oil Content: {results.oilContent}</div>
-      <div className="result-item">Confidence: {results.confidence}</div>
-      <div className="result-item">Health Rating: {results.healthRating}</div>
-      <div className="result-item">
-        Interesting Fact: {results.interestingFact}
+      <div className="result-item interesting-fact">
+        💡 {results.interestingFact}
       </div>
     </div>
   );
